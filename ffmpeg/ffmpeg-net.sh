58c0c24a47f5d95fe8be20c1c7bea0ced948bb6e
mssaleh <mssaleh@live.com>
2020-06-14 12:51:50 +0400

    fixes

--- ffmpeg/ffmpeg-net.sh
+++
@@ -1,9 +0,0 @@
-                sudo apt purge -y ffmpeg
-                sudo apt install -y libavcodec-extra58 libavdevice58 libavfilter-extra7 libavformat58 libavutil56 libc6 \
-                libffms2-4 libgpac4 libpostproc55 libsdl2-2.0-0 libstdc++6 libswresample3 libswscale5 libx264-155 libx265-179 x264 x265
-                sudo curl -L "https://raw.githubusercontent.com/mssaleh/binaries/master/ffmpeg/ffmpeg" -o /usr/local/bin/ffmpeg
-                sudo curl -L "https://raw.githubusercontent.com/mssaleh/binaries/master/ffmpeg/ffplay" -o /usr/local/bin/ffplay
-                sudo curl -L "https://raw.githubusercontent.com/mssaleh/binaries/master/ffmpeg/ffprobe" -o /usr/local/bin/ffprobe
-                sudo chmod +x /usr/local/bin/ffmpeg
-                sudo chmod +x /usr/local/bin/ffplay
-                sudo chmod +x /usr/local/bin/ffprobe
